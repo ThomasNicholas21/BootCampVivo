316fb692cbc2aa3ba2362ec765414df9f87e0f03
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-06-11 20:54:57 -0300

    Estudando modo iterativo: BootCamp

--- primeiraetapa/modo_interativo.py
+++ primeiraetapa/modo_interativo.py
@@ -1,2 +1,12 @@
 #Este arquivo é destinado para estudo junto ao bootcampVivo 
 #Terá anotações e código
+
+
+# Modo iterativo é um modo que possibilita executar em tempo real o código
+# Somente digitar python no terminal
+# Serve para realizar teste rápidos
+# Quando executa python -i <nome programa> seleciona um progrma
+
+print(dir(1)) # Mostra atributos válidos no modo iterativo
+
+print(help(int)) # Quais argumentos recebe e o que retorna, sair do help "enter"
