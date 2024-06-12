93b8f517aed5fcc0e2d6a4fee30abfe3b2a55f00
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-06-11 21:28:48 -0300

    Estudando Váriaveis e Constantes: BootCamp

--- primeiraetapa/variaveis_constantes.py
+++ primeiraetapa/variaveis_constantes.py
@@ -1,2 +1,31 @@
 #Este arquivo é destinado para estudo junto ao bootcampVivo 
 #Terá anotações e código
+
+# Variáveis são recebidas e não necessáriamente 
+# permanecem com o mesmo valor
+# Constante é imutável, comeca com mesmo valor e permanece com mesmo.
+# Não existe no python, porém por concao é deixar em maaiúsculo
+
+
+#Boas práticas: "snake case", nome sugestivos, constantes maaiúsculo
+
+
+# Exemplos de variáveis
+def print_dados(nome, idade, email):
+    return print(f'Seu nome é {nome} e tem {idade} anos. Seu e-mail de contato é {email}.')
+
+# Exemplos de variáveis 
+nome = 'Thomas'
+idade = 23
+email = 'thomasnicholaas@gmail.com'
+
+print_dados(nome, idade,email)
+
+nome = 'Guilherme'
+idade = 28
+email = 'guilherme@gmail.com'
+
+print_dados(nome, idade, email)
+
+
+
