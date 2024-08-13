881dc63fd0298fdeabbc09410bf403819b9d9ed3
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-08-13 20:26:52 -0300

    Sistema de gerenciamento para biblioteca

--- poo/1exercicio/biblioteca_gerenciamento.py
+++ poo/1exercicio/biblioteca_gerenciamento.py
@@ -2,7 +2,6 @@
 # Objetivo: Criar um sistema simples para gerenciar uma biblioteca, utilizando classes, objetos, métodos construtores e destrutores.
 # Descrição: Você irá criar três classes principais: Livro, Usuario, e Biblioteca.
 
-# Atributos: titulo (string) autor (string) ano_publicacao (int) disponivel (boolean, inicializado como True)
 class Livro: # Construtor que inicializa todos os atributos do livro.
     def __init__(self, titulo, autor, ano_publicacao, disponivel=True):
         self.titulo = str(titulo)
@@ -10,49 +9,46 @@ class Livro: # Construtor que inicializa todos os atributos do livro.
         self.ano_publicacao = int(ano_publicacao)
         self.disponivel = bool(disponivel)
 
-# Método para marcar o livro como emprestado (disponivel se torna False).
     def livro_emprestado(self):
-        self.disponivel = False
-        return self.disponivel
+        if self.disponivel == False:
+            return f'Livro {self.titulo} Está sendo utilizado.'
+        return f'Livro {self.titulo} Disponivel.'
 
-# Método para marcar o livro como devolvido (disponivel se torna True).
     def livro_disponivel(self):
-        self.disponivel = True
-        return self.disponivel
+        if self.disponivel == True:
+            return f'Livro {self.titulo} disponível.'
+        return f'Livro {self.titulo} indiponível.'
+    
+    def __str__(self):
+        return f'Livro: {self.titulo}, Autor: {self.autor}, Publicacao: {self.ano_publicacao}, Disponível: {self.disponivel}'
 
-# Método destrutor que imprime uma mensagem quando um objeto Livro é destruído.
-    def __del__(self):
-        print(f'{self.titulo} está sendo excluido.')
+    def __del__(self): # O mesmo não é necessário, pois o python possui um coletor de lixo
+        pass
 
-# Atributos: nome (string) livros_emprestados (lista de objetos Livro, inicializada como uma lista vazia)
 class Usuario(Livro): # Construtor que inicializa o nome do usuário.
     def __init__(self, nome, livros_emprestados=[]):
         self.nome = str(nome)
         self.livros_emprestados = list(livros_emprestados)
 
-# Método para emprestar um livro (adiciona o livro à lista livros_emprestados e marca o livro como emprestado).
     def emprestar_livro(self, livros_emprestados):
         emprestimo = self.livro_emprestado()
         livros_emprestados.append(emprestimo)
 
-# Método para devolver um livro (remove o livro da lista livros_emprestados e marca o livro como disponível).
     def receber_livro(self, livros_emprestados):
         emprestimo = self.livro_disponivel()
         livros_emprestados.remove(emprestimo)
 
-# Método destrutor que imprime uma mensagem quando um objeto Usuario é destruído.
     def __del__(self):
         print(f'{self.nome}: Usuário destruidos.')
     
-# Atributos: livros (lista de objetos Livro, inicializada como uma lista vazia) usuarios (lista de objetos Usuario, inicializada como uma lista vazia)
-class Biblioteca: # Construtor que inicializa as listas de livros e usuários.
+class Biblioteca(Livro, Usuario): 
     def __init__(self, livros=[], usuarios=[]):
         self.livros = livros
         self.usuarios = usuarios
 
 # Método para adicionar um livro à biblioteca.
     def adiciona_livro(self, livros):
-        ...
+        livros.append(Livro)
 # Método para adicionar um usuário à biblioteca.
     def adicionar_usuario(self, usuarios):
         ...
@@ -72,3 +68,11 @@ class Biblioteca: # Construtor que inicializa as listas de livros e usuários.
 # Adicione os livros e usuários à Biblioteca.
 # Faça alguns testes de empréstimo e devolução de livros.
 # Liste os livros disponíveis e os usuários cadastrados para verificar se tudo está funcionando corretamente.
+
+
+livro1 = Livro('Cosmos', 'Carl Segan', 1980, True)
+livro2 = Livro('Rápido e Devagar', 'Daniel Kahneman', 2011, False)
+print(livro1.livro_disponivel())
+print(livro2.livro_disponivel())
+print(livro1.livro_emprestado())
+print(livro2.livro_emprestado())
