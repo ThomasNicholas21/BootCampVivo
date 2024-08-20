03d89854a575ab99a92e07626916ae375cbd9c89
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-08-20 16:08:48 -0300

    Herança Multipla

--- poo/heranca/henrança_multipla.py
+++ poo/heranca/henrança_multipla.py
@@ -1,16 +1,31 @@
 class animal:
-    pass
+    def __init__(self, num_patas):
+        self.num_patas = num_patas
+
+    def __str__(self):
+        return f'{self.__class__.__name__}: {' '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}'
 
 class mamifero(animal):
-    pass
+    def __init__(self, cor_pelo, **kwargs): # **kwargs - todo argumento novo na classe animal sera adicionado caso utilizar o "**kwargs"
+        super().__init__(**kwargs)
+        self.cor_pelo = cor_pelo
 
 class ave(animal):
+    def __init__(self, cor_bico, **kwargs):
+        super().__init__(**kwargs)
+        self.cor_bico = cor_bico
+        
+        
+
+class Cachorro(mamifero):
     pass
 
-class cachorro(mamifero):
+class Ornitorrinco(mamifero, ave):
     pass
 
-class ornitorrinco(mamifero, ave):
-    pass
+cachorro = Cachorro(num_patas=4, cor_pelo='Azul') # Utilizando chave e valor devido ao kwargs
+print(cachorro)
+ornitorrinco = Ornitorrinco(num_patas=4, cor_pelo='Preto', cor_bico='Amarelo')
+print(ornitorrinco)
 
 
