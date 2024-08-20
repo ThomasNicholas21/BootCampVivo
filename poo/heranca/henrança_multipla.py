f6fce654f8a864ec604ccc4f1f5523ba24c3bbcc
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-08-19 21:40:21 -0300

    Hernaça Multipla

---
+++ poo/heranca/henrança_multipla.py
@@ -0,0 +1,16 @@
+class animal:
+    pass
+
+class mamifero(animal):
+    pass
+
+class ave(animal):
+    pass
+
+class cachorro(mamifero):
+    pass
+
+class ornitorrinco(mamifero, ave):
+    pass
+
+
